84391cad932f9407c1b1a2449aa7b9136633b62d
luc-georges <luc.georges.ld@gmail.com>
2020-08-19 22:43:04 +0200

    better use of count for the top users view

--- APIrest/migrations/deploy/05-view-global-users.sql
+++ APIrest/migrations/deploy/05-view-global-users.sql
@@ -5,17 +5,16 @@ BEGIN;
 
 CREATE VIEW getTopusers AS
 
-        SELECT DISTINCT ON (e.user_id) "user_id" AS "_user_id",
-        us.nickname AS "_name",
-        us.avatar AS "_avatar",
-        us.banner AS "_banner",
-        (SELECT COUNT(id) FROM user_access."event" ev WHERE us.id = ev.user_id) AS "_total_events",
-        (SELECT COUNT (CASE WHEN g.id = 1 THEN 1 END) ) AS "_total_cs",
-        (SELECT COUNT (CASE WHEN g.id = 2 THEN 1 END) ) AS "_total_lol"
-    FROM user_access."user" us
-    JOIN user_access."event" e  ON us.id = e.user_id 
-    JOIN user_access."game"  g ON  e.game_id = g.id
-    GROUP BY  e.user_id, us.nickname, us.id ;
-
-
+        SELECT  us."id" AS "_user_id",
+                us.nickname AS "_name",
+                us.avatar AS "_avatar",
+                us.banner AS "_banner",
+                COUNT(DISTINCT e.id) as "_total_events",
+                COUNT(*) FILTER (WHERE g.id = 1 ) AS "_total_cs",
+                COUNT(*) FILTER (WHERE g.id = 2) AS "_total_lol"
+            FROM user_access."user" us 
+            JOIN user_access."event" e ON us.id = e.user_id 
+            JOIN user_access."game"  g ON e.game_id = g.id
+        GROUP BY us.id 
+        ORDER BY "_total_events" DESC;
 COMMIT;
