159ca833f7334b5e74aceed2e79a3e29a356f75d
luc-georges <luc.georges.ld@gmail.com>
2020-08-19 09:26:49 +0200

    update Event function to add lang label and flag

--- APIrest/migrations/deploy/04-function-event.sql
+++ APIrest/migrations/deploy/04-function-event.sql
@@ -12,10 +12,12 @@ CREATE type event_data as ( "event_id" INT ,
                             "duration" INTERVAL,
                             "player_count" INT,
                             "description" TEXT,
+                            "lang_label" TEXT,
+                            "flag" TEXT,
                             "status" INT,
                             "vocal" TEXT,
                             "level_id" INT,
-                            "label" TEXT  );
+                            "label" TEXT );
 
 -- Function to get all DAtas from a specifique event param: (event_id)
 CREATE FUNCTION getEventData("EVENT_ID" INT)
@@ -31,7 +33,9 @@ SELECT e.id AS "event_id",
         e.event_time AS "starting",
         e.duration , 
         e.player_count,
-         e.description, 
+         e.description,
+         la.label,
+         la.icon, 
          e.status, 
          e.vocal,
          l.id,
@@ -39,6 +43,8 @@ SELECT e.id AS "event_id",
     FROM user_access."event" e
     JOIN user_access."user" u ON u.id = e.user_id
     JOIN user_access."game" g ON g.id = e.game_id
+    JOIN user_access."M_EVENT_has_LANG" ehl ON e.id = ehl.event_id
+    JOIN user_access."lang" la ON la.id = ehl.lang_id
     JOIN user_access."M_USER_has_GAME" uhg ON u.id = uhg.user_id AND uhg.game_id = e.game_id
     JOIN user_access."level" l ON l.id = uhg.level_id
     WHERE e.id = "EVENT_ID";
@@ -62,7 +68,9 @@ SELECT e.id AS "event_id",
         e.event_time AS "starting",
         e.duration , 
         e.player_count,
-         e.description, 
+         e.description,
+         la.label,
+         la.icon, 
          e.status, 
          e.vocal,
          l.id,
@@ -70,6 +78,8 @@ SELECT e.id AS "event_id",
     FROM user_access."event" e
     JOIN user_access."user" u ON u.id = e.user_id
     JOIN user_access."game" g ON g.id = e.game_id
+    JOIN user_access."M_EVENT_has_LANG" ehl ON e.id = ehl.event_id
+    JOIN user_access."lang" la ON la.id = ehl.lang_id
     JOIN user_access."M_USER_has_GAME" uhg ON u.id = uhg.user_id AND uhg.game_id = e.game_id
     JOIN user_access."level" l ON l.id = uhg.level_id
 
