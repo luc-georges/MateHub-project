d90ebeea0f706504e8d0d795760b92f6bd0baa3b
luc-georges <luc.georges.ld@gmail.com>
2020-08-21 12:59:45 +0200

    unknown change

--- APIrest/index.js
+++ APIrest/index.js
@@ -24,7 +24,7 @@ app.use(session({
 app.use(router);
 app.use(routerNews);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, _ => {
 console.log(`Running on ${port}`)
