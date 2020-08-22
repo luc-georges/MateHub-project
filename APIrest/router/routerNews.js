a6d63d7e91fb45acd673e2bbd8101a9d7e8800de
luc-georges <luc.georges.ld@gmail.com>
2020-08-22 09:57:49 +0200

    api news rework

--- APIrest/router/routerNews.js
+++ APIrest/router/routerNews.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const routerNews = express.Router();
 const lolNewsController = require('../controllers/newsApi/lolNewsController');
+const url = require('url');
 
-routerNews.get('/lol/news', lolNewsController.getLolNews);
+routerNews.get('/lol/news',function(req, res) {
+    res.redirect(url.format({
+       pathname:"https://simplescraper.io/api/eUlWk5o7c0B7pFmviJ8O",
+       query: {
+          "apikey": "4AvTfAaCXLRdueCL0nlyT4KTCvcHVBZB",
+          "offset": 0,
+          "limit":10
+       }}))});
+
+       
 
 module.exports = routerNews;
 
